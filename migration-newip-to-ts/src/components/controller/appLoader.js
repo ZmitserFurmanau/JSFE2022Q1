8579eedcd899b985da707cc3089e9495cc002732
ZmitserFurmanau <zmitserfurmanau@gmail.com>
2022-07-03 18:22:21 +0300

    feat: add key in appLoader.js from https://newsapi.org and rename project, user in package.json files

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'b8dbe75de98747828758ff4b90f1d1ca', // получите свой ключ https://newsapi.org/
         });
     }
 }
