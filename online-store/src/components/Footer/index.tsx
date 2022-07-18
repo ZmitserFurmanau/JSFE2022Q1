b9d779d413c7540ba16a5cd2fb32730e4275fc82
ZmitserFurmanau <zmitserfurmanau@gmail.com>
2022-07-18 14:57:19 +0300

    fix: update css files for mobile version site

--- online-store/src/components/Footer/index.tsx
+++ online-store/src/components/Footer/index.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import styles from './Footer.module.scss';
-import { Link } from 'react-router-dom';
 
 export const Footer: React.FC = () => {
   return (
     <div className={styles.footer}>
       <div className={styles.container}>
-        <Link to="https://github.com/ZmitserFurmanau" target="_blank">
+        <a href="https://github.com/zmitserfurmanau" target="_blank" rel="noopener noreferrer">
           <div>
             <img width="38" src="./img/github.svg" alt="Github logo" />
           </div>
-        </Link>
+        </a>
         <div className={styles.year}>
           <p>© 2022 Zmitser Furmanau</p>
         </div>
-        <Link to="https://rs.school/js/" target="_blank">
+        <a href="https://rs.school/js/" target="_blank" rel="noopener noreferrer">
           <div>
             <img width="86" src="./img/rs_school_js.svg" alt="RSSchool logo" />
           </div>
-        </Link>
+        </a>
       </div>
     </div>
   );
